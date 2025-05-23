933d9525d7d2d8acec6d99e675a3a5a9b0a2451e
Anushka <nicepersonanushka@gmail.com>
2025-05-23 23:57:35 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
 const express=require('express');
-
 const app=express();
 
-app.get("/sum",function(req,res){
+app.use(express.json());
 
-    const a=parseInt(req.query.a);
-    const b=parseInt(req.query.b);
+const cors = require('cors');
+app.use(cors());
+
+
+app.post("/sum",(req,res)=>{
+    const a=parseInt(req.body.a);
+    const b=parseInt(req.body.b);
 
     res.json({
         ans:a+b
     })
-
-});
-
-app.get("/subtract",function(req,res){
-
-    const a=parseInt(req.query.a);
-    const b=parseInt(req.query.b);
-
-    if(a>=b){
-        res.json({
-            ans:a-b
-        })
-    }
-    else{
-        res.json({
-            ans:b-1
-        })
-    }
-
-});
-
-app.get("/multiply",function(req,res){
-    const a=parseInt(req.query.a);
-    const b=parseInt(req.query.b);
-    res.json({
-        ans:a*b
-    })
-});
-
-app.get("/divide",function(req,res){
-    const a=parseInt(req.query.a);
-    const b=parseInt(req.query.b);
-
-    res.json({
-        ans:a/b
-    })
-});
-
-app.listen(8000,()=>{
-    console.log('Server is running on port 8000');
 })
+
+app.listen(5000);
+
